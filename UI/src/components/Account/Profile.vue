93201b19b582502d76c64338388326029d4230fd
黄杰 <821486692@qq.com>
2020-09-02 00:02:26 +0800

    优化界面

--- UI/src/components/Account/Profile.vue
+++ UI/src/components/Account/Profile.vue
@@ -1,52 +1,61 @@
 <template>
 <div>
-  <el-tabs type="border-card">
-    <el-tab-pane label="基础信息">
-      <div>
-        <el-upload class="avatar-uploader" action="https://jsonplaceholder.typicode.com/posts/" :show-file-list="false" :on-success="handleAvatarSuccess" :before-upload="beforeAvatarUpload">
-          <img v-if="imageUrl" :src="imageUrl" class="avatar">
-          <i v-else class="el-icon-plus avatar-uploader-icon"></i>
-        </el-upload>
-        <span class="avatar-remind">(点击上传头像)</span>
-      </div>
-    </el-tab-pane>
-    <el-tab-pane label="修改密码">角色管理</el-tab-pane>
-  </el-tabs>
+    <el-tabs type="border-card">
+        <el-tab-pane label="基础信息" style="width:450px">
+            <div>
+                <el-upload class="avatar-uploader" action="https://jsonplaceholder.typicode.com/posts/" :show-file-list="false" :on-success="handleAvatarSuccess" :before-upload="beforeAvatarUpload">
+                    <img v-if="imageUrl" :src="imageUrl" class="avatar">
+                    <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+                </el-upload>
+                <span class="avatar-remind">(点击上传头像)</span>
+            </div>
+            <el-form ref="form" :model="form" label-width="80px">
+                <el-form-item label="姓名">
+                    <el-input v-model="form.userName"></el-input>
+                </el-form-item>
+                <el-form-item label="手机号">
+                    <el-input v-model="form.mobilePhone"></el-input>
+                </el-form-item>
+            </el-form>
+        </el-tab-pane>
+        <el-tab-pane label="修改密码">角色管理</el-tab-pane>
+    </el-tabs>
 </div>
 </template>
 
 <script>
 export default {
-  name: 'Profile',
-  data() {
-    return {
-      imageUrl: ''
-    };
-  },
-  methods: {
-    handleAvatarSuccess(res, file) {
-      this.imageUrl = URL.createObjectURL(file.raw);
+    name: 'Profile',
+    data() {
+        return {
+            imageUrl: '',
+            form: {}
+        };
     },
-    beforeAvatarUpload(file) {
-      const isJPG = file.type === 'image/jpeg';
-      const isLt2M = file.size / 1024 / 1024 < 2;
+    methods: {
+        handleAvatarSuccess(res, file) {
+            this.imageUrl = URL.createObjectURL(file.raw);
+        },
+        beforeAvatarUpload(file) {
+            const isJPG = file.type === 'image/jpeg';
+            const isLt2M = file.size / 1024 / 1024 < 2;
 
-      if (!isJPG) {
-        this.$message.error('上传头像图片只能是 JPG 格式!');
-      }
-      if (!isLt2M) {
-        this.$message.error('上传头像图片大小不能超过 2MB!');
-      }
-      return isJPG && isLt2M;
+            if (!isJPG) {
+                this.$message.error('上传头像图片只能是 JPG 格式!');
+            }
+            if (!isLt2M) {
+                this.$message.error('上传头像图片大小不能超过 2MB!');
+            }
+            return isJPG && isLt2M;
+        }
     }
-  }
 }
 </script>
 
 <style scoped>
 /deep/ .avatar-remind {
-  display: block;
-  text-align: center;
-  font-size: 0.8rem;
+    display: block;
+    text-align: center;
+    font-size: 0.8rem;
 }
 </style>
